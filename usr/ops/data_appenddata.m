cea9c8e4805eee8b370370d7e50e488e9c64506a
zhengkaiyu <k.zheng@ucl.ac.uk>
2017-05-16 22:37:10 +0100

    append data updated
    allow data append to all dimensions, including non-empty ones, as long
    as other dimenions are equal in size

--- usr/ops/data_appenddata.m
+++ usr/ops/data_appenddata.m
@@ -12,46 +12,47 @@ try
     % get all selected data dimensions and check
     dim_check=arrayfun(@(x)obj.data(x).datainfo.data_dim,selected_data,'UniformOutput',false);
     dim_master=dim_check{1};
-    dim_check=cell2mat(cellfun(@(x)dim_master~=x,dim_check,'UniformOutput',false)');
-    % find if there is any mismatch, should be all zero if matches
-    [r,c,~]=find(dim_check);
-    if isempty(r)
-        % find any free dimension
-        free_dim=obj.DIM_TAG(dim_master==1);
-        if isempty(free_dim)
-            free_dim={'T'};% if we are full suggest append in T
+    % find any free dimension
+    free_dim=obj.DIM_TAG(dim_master==1);
+    if isempty(free_dim)
+        free_dim={'T'};% if we are full suggest append in T
+    end
+    % ask append in which dimension
+    % get binning information
+    prompt = {sprintf('Append data\n%s in which dimension (t/X/Y/Z/T): ',sprintf('%s\n',obj.data(selected_data).dataname))};
+    dlg_title = 'Append Dimension';
+    num_lines = 1;
+    def = free_dim(1);
+    set(0,'DefaultUicontrolBackgroundColor',[0.3,0.3,0.3]);
+    set(0,'DefaultUicontrolForegroundColor','k');
+    answer = inputdlg(prompt,dlg_title,num_lines,def);
+    set(0,'DefaultUicontrolBackgroundColor','k');
+    set(0,'DefaultUicontrolForegroundColor','w');
+    if isempty(answer)
+        % action cancelled
+        append_dim=[];
+    else
+        switch answer{1}
+            case {'t','X','Y','Z','T'}
+                append_dim=strfind(cell2mat(obj.DIM_TAG),answer{1});
+            otherwise
+                % invalid input default to 1st free_dim
+                append_dim=strfind(cell2mat(obj.DIM_TAG),free_dim(1));
         end
-        % ask append in which dimension
-        % get binning information
-        prompt = {sprintf('Append data\n%s in which dimension (t/X/Y/Z/T): ',sprintf('%s\n',obj.data(selected_data).dataname))};
-        dlg_title = 'Append Dimension';
-        num_lines = 1;
-        def = free_dim(1);
-        set(0,'DefaultUicontrolBackgroundColor',[0.3,0.3,0.3]);
-        set(0,'DefaultUicontrolForegroundColor','k');
-        answer = inputdlg(prompt,dlg_title,num_lines,def);
-        set(0,'DefaultUicontrolBackgroundColor','k');
-        set(0,'DefaultUicontrolForegroundColor','w');
-        if isempty(answer)
-            % action cancelled
-            append_dim=[];
-        else
-            switch answer{1}
-                case {'t','X','Y','Z','T'}
-                    append_dim=strfind(cell2mat(obj.DIM_TAG),answer{1});
-                otherwise
-                    % invalid input default to 1st free_dim
-                    append_dim=strfind(cell2mat(obj.DIM_TAG),free_dim(1));
-            end
-        end
-        
-        if isempty(append_dim)
-            message=sprintf('data append operation cancelled\n');
-        else
+    end
+    if isempty(append_dim)
+        message=sprintf('data append operation cancelled\n');
+    else
+        % check all non-appending dim has the same dimensions
+        dim_check=cell2mat(cellfun(@(x)dim_master~=x,dim_check,'UniformOutput',false)');
+        % find if there is any mismatch, should be all zero if matches
+        [r,c,~]=find(dim_check(~append_dim));
+        if isempty(r)
             % --- calculation part ---
             parent_data=selected_data(1);% treat the first one of selected as parent
             % add new data
-            obj.data_add(cat(2,'data_append|',obj.data(parent_data).dataname),[],[]);
+            newname=sprintf('%s+',obj.data(selected_data).dataname);
+            obj.data_add(cat(2,'data_append|(',newname(1:end-1),')'),[],[]);
             % get new data index
             current_data=obj.current_data;
             % pass on metadata info
@@ -71,8 +72,17 @@ try
             for dim_idx=1:numel(obj.DIM_TAG)
                 dim=obj.DIM_TAG{dim_idx};
                 if dim_idx==append_dim
-                    obj.data(current_data).datainfo.(cat(2,'d',dim))=1;
-                    obj.data(current_data).datainfo.(dim)=linspace(0,new_dim_size-1,new_dim_size);
+                    if obj.data(parent_data).datainfo.(cat(2,'d',dim))~=0
+                        obj.data(current_data).datainfo.(cat(2,'d',dim))=obj.data(parent_data).datainfo.(cat(2,'d',dim));
+                        if obj.data(selected_data(2)).datainfo.(dim)(1)>obj.data(selected_data(1)).datainfo.(dim)(end)
+                            obj.data(current_data).datainfo.(dim)=[obj.data(selected_data(1)).datainfo.(dim);obj.data(selected_data(2)).datainfo.(dim)];
+                        else
+                            obj.data(current_data).datainfo.(dim)=[obj.data(selected_data(1)).datainfo.(dim);obj.data(selected_data(2)).datainfo.(dim)+obj.data(selected_data(1)).datainfo.(dim)(end)+obj.data(current_data).datainfo.(cat(2,'d',dim))];
+                        end
+                    else
+                        obj.data(current_data).datainfo.(cat(2,'d',dim))=1;                    
+                        obj.data(current_data).datainfo.(dim)=linspace(0,new_dim_size-1,new_dim_size);
+                    end
                 else
                     obj.data(current_data).datainfo.(dim)=obj.data(parent_data).datainfo.(dim);
                     obj.data(current_data).datainfo.(cat(2,'d',dim))=obj.data(parent_data).datainfo.(cat(2,'d',dim));
@@ -84,10 +94,10 @@ try
             message=sprintf('%s appended into %s\n',sprintf('%s\n',...
                 obj.data(selected_data).dataname),...
                 obj.data(current_data).dataname);
+        else
+            % one of the data has mismatched dimension
+            message=sprintf('data %s has mismatched %s dimensions\n',selected_data(r),obj.DIM_TAG{c});
         end
-    else
-        % one of the data has mismatched dimension
-        message=sprintf('data %s has mismatched %s dimensions\n',selected_data(r),obj.DIM_TAG{c});
     end
 catch exception
     message=exception.message;
